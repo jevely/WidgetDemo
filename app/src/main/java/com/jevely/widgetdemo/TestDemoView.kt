c1174201536c9f940dc5c174c006de83a9363acb
jevely <lijiawei_855@126.com>
2022-04-07 07:52:00 +0800

    feat: 项目搭建完成

---
+++ app/src/main/java/com/jevely/widgetdemo/TestDemoView.kt
@@ -0,0 +1,33 @@
+package com.jevely.widgetdemo
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
+import android.util.AttributeSet
+import android.view.View
+
+class TestDemoView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    val paint = Paint()
+
+    init {
+        paint.isAntiAlias = true //打开抗锯齿
+
+        paint.style = Paint.Style.FILL //内部填充
+        paint.style = Paint.Style.FILL_AND_STROKE //内部填充和描边
+        paint.style = Paint.Style.STROKE //描边
+
+        paint.strokeWidth = 10f //描边宽度，只有style是FILL_AND_STROKE或者STROKE时有用
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+    }
+
+}
