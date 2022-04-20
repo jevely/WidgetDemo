5ecc83f6abee1a241679e5c87cf8bdaa116fd7a2
jevely <lijiawei_855@126.com>
2022-04-21 07:47:24 +0800

    feat: 添加注释

--- app/src/main/java/com/jevely/widgetdemo/widget/TestObjectAnimatorView.kt
+++ app/src/main/java/com/jevely/widgetdemo/widget/TestObjectAnimatorView.kt
@@ -10,6 +10,9 @@ class TestObjectAnimatorView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : androidx.appcompat.widget.AppCompatImageView(context, attrs) {
 
+    /**
+     * ObjectAnimator会通过反射调用该方法
+     */
     fun setDrag(dragX: Float) {
         translationX = dragX
     }
